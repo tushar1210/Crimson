b22501a9185421153afa43c68822877f3d3f1ce5
tushar1210 <ts26192271@gmail.com>
2019-02-23 05:27:02 +0530

    XD + Classifier Added

---
+++ Crimson/Q1VC.swift
@@ -0,0 +1,28 @@
+//
+//  Q1VC.swift
+//  Crimson
+//
+//  Created by Tushar Singh on 23/02/19.
+//  Copyright © 2019 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+
+class Q1VC: UIViewController {
+
+    @IBOutlet weak var subView: UIView!
+    
+    @IBOutlet weak var responseTF: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        subView.layer.cornerRadius = 15
+        
+    }
+    
+
+    
+
+  
+
+}
---
+++ Crimson/Q3VC.swift
@@ -0,0 +1,30 @@
+//
+//  Q3VC.swift
+//  Crimson
+//
+//  Created by Tushar Singh on 23/02/19.
+//  Copyright © 2019 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+
+class Q3VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
